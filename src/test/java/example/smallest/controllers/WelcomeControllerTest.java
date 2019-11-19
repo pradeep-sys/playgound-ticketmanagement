0d2b572c7868c744e92a8ac206675e09b436ec69
Indranil Banerjee <ibanerjee@altimetrik.com>
2019-11-20 01:31:02 +0530

    comit

--- src/test/java/example/smallest/controllers/WelcomeControllerTest.java
+++ src/test/java/example/smallest/controllers/WelcomeControllerTest.java
@@ -2,14 +2,12 @@ package example.smallest.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WelcomeControllerTest {
 	
-	@Mock
-	WelcomeController welcomeControllerTest;
+	
 	@Test
 	public void assertionTest() {
 	
